16aa8c63a7ce76cb42abff8eddc4481aa09553eb
unknown <mdsabuj1925@.com>
2025-05-02 19:00:05 +0600

    project update

--- js/script.js
+++ js/script.js
@@ -22,6 +22,72 @@ $(function () {
         $(".progress-bar").ProgressBar();
     });
 
+    // brand slider
+    $('.testimonial_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+
+        nextArrow: '<i class="fas fa-long-arrow-alt-right nextArrow"></i>',
+        prevArrow: '<i class="fas fa-long-arrow-alt-left prevArrow"></i>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+
+
+    // small device menu icon
+    $(".navbar-toggler").on("click", function () {
+        $(".navbar-toggler").toggleClass("show");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
